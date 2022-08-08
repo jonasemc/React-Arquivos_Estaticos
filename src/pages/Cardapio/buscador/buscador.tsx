93a863f726c6e77267475c5a4e2bb1e6d68cdeb0
Jonas <jonasmirandaa21@gmail.com>
2022-08-08 10:52:47 -0300

    feat: create search component

---
+++ src/pages/Cardapio/buscador/buscador.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from "./buscador.module.scss";
+import { CgSearch } from "react-icons/cg";
+
+interface Props {
+  busca: string;
+  setBusca: React.Dispatch<React.SetStateAction<string>>;
+  //{dispara uma acao}Dispatch<{acao disparada}React.SetStateAction<{o que vai ser passado dentro do setState}string>>
+}
+
+export default function Buscador({ busca, setBusca }: Props) {
+  return (
+    <div className={styles.buscador}>
+      <input
+        value={busca}
+        onChange={(evento) => setBusca(evento.target.value)}
+      />
+      <CgSearch size={20} color="#4c4d5e" />
+    </div>
+  );
+}
