fedbeda65e2e69775b358f7992aec6c65247692f
jonas <jonasmirandaa21@gmail.com>
2022-08-15 15:39:13 -0300

    feat: create filter component

--- src/pages/Cardapio/Cardapio.tsx
+++ src/pages/Cardapio/Cardapio.tsx
@@ -2,6 +2,7 @@ import styles from "./Cardapio.module.scss";
 import { ReactComponent as Logo } from "assets/logo.svg";
 import Buscador from "./buscador/buscador";
 import { useState } from "react";
+import Filtros from "./filtros/filtro";
 
 export default function Cardapio() {
   const [busca, setBusca] = useState("");
@@ -17,6 +18,9 @@ export default function Cardapio() {
       <section className={styles.cardapio}>
         <h3 className={styles.cardapio__titulo}>Cardapio</h3>
         <Buscador busca={busca} setBusca={setBusca} />
+        <div className={styles.cardapio__filtros}>
+          <Filtros></Filtros>
+        </div>
       </section>
     </main>
   );
